505d60303ec922e0d8e0075406258c520a5e3932
inesDrebenstedt <54622994+inesDrebenstedt@users.noreply.github.com>
2025-07-15 14:52:16 +0200

    Changed Lab03.sh file

--- Lab03.sh
+++ Lab03.sh
@@ -1,22 +1,38 @@
- cat <<EOF > k8s-lab03.sh
+cat <<'EOF' > k8s-lab03.sh
 #!/bin/bash
- echo "-----> 1. Download files"
- git clone -b main https://github.com/asokone/Kube_Fundamentals.git read -p "Hit ENTER for next step." 
-  echo "-----> 1. cd to yaml"
- cd Kube_Fundamentals/pods/lab03 read -p "Hit ENTER for next step." 
-  echo "-----> 1. Create pod"
- kubectl create -f mypod-03.yaml ; kubectl get pods read -p "Hit ENTER for next step." 
-  echo "-----> 1. Describe pod ns"
- kubectl describe pod mypod-03 | grep -i namespace read -p "Hit ENTER for next step." 
-  echo "-----> 1. Describe pod"
- kubectl describe pod mypod-03 read -p "Hit ENTER for next step." 
-  echo "-----> 1. Describe pods in detail"
- kubectl get pods -o wide read -p "Hit ENTER for next step." 
-  echo "-----> 1. Curl the pod at port 80"
- curl <pod_IP>:80/ read -p "Hit ENTER for next step." 
-  echo "-----> 1. Delete pod"
- kubectl delete -f mypod-03.yaml read -p "Hit ENTER for next step." 
-  echo "-----> 1. Deleted pod is not listed anymore:"
- kubectl get pods -o wide
- 
- EOF
+echo "-----> 1. Download files"
+git clone -b main https://github.com/asokone/Kube_Fundamentals.git
+read -p "Hit ENTER for next step."
+
+echo "-----> 2. cd to yaml"
+cd Kube_Fundamentals/pods/lab03
+read -p "Hit ENTER for next step."
+
+echo "-----> 3. Create pod"
+kubectl create -f mypod-03.yaml ; kubectl get pods
+read -p "Hit ENTER for next step."
+
+echo "-----> 4. Describe pod ns"
+kubectl describe pod mypod-03 | grep -i namespace
+read -p "Hit ENTER for next step."
+
+echo "-----> 5. Describe pod"
+kubectl describe pod mypod-03
+read -p "Hit ENTER for next step."
+
+echo "-----> 6. Describe pods in detail"
+kubectl get pods -o wide
+read -p "Hit ENTER for next step."
+
+echo "-----> 7. Curl the pod at port 80"
+POD_IP=$(kubectl get pod mypod-03 -o jsonpath='{.status.podIP}')
+curl http://$POD_IP:80/
+read -p "Hit ENTER for next step."
+
+echo "-----> 8. Delete pod"
+kubectl delete -f mypod-03.yaml
+read -p "Hit ENTER for next step."
+
+echo "-----> 9. Deleted pod is not listed anymore:"
+kubectl get pods -o wide
+EOF
